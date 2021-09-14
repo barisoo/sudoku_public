6f36d14b8f0809495a7489f9c91136661a016cb5
barisoo <bariso@mac.com>
2021-09-15 01:05:51 +1000

    Update numberpad.js

--- numberpad.js
+++ numberpad.js
@@ -1,25 +1,25 @@
 let numberpad = Vue.createApp({
 	template: `<table width="360">
   			<tr>
-  				<td><button> 1</button> </td>
-  				<td><button> 2</button> </td>
-  				<td><button> 3</button> </td>
+  				<td><button onclick="insertValFromInput(1)"> 1</button> </td>
+  				<td><button onclick="insertValFromInput(2)"> 2</button> </td>
+  				<td><button onclick="insertValFromInput(3)"> 3</button> </td>
   			</tr>
   			<tr>
-  				<td> <button>4 </button></td>
-  				<td> <button>5 </button></td>
-  				<td> <button>6 </button></td>
+  				<td> <button onclick="insertValFromInput(4)">4 </button></td>
+  				<td> <button onclick="insertValFromInput(5)">5 </button></td>
+  				<td> <button onclick="insertValFromInput(6)">6 </button></td>
   			</tr>
   			<tr>
-  				<td> <button>7</button> </td>
-  				<td> <button>8</button> </td>
-  				<td> <button>9</button> </td>
+  				<td> <button onclick="insertValFromInput(7)">7</button> </td>
+  				<td> <button onclick="insertValFromInput(8)">8</button> </td>
+  				<td> <button onclick="insertValFromInput(9)">9</button> </td>
   			</tr>
 
   			<tr>
-  				<td><button class = "numberpad-control"> Enter</button> </td>
-  				<td><button class = "numberpad-control"> Erase</button> </td>
-  				<td><button class = "numberpad-control"> Note</button> </td>
+  				<td><button class = "numberpad-control" onclick="shouldCheckAnswers =!shouldCheckAnswers"> Check</button> </td>
+  				<td><button class = "numberpad-control" onclick="insertValFromInput(0)"> Erase</button> </td>
+  				<td><button class = "numberpad-control" onclick="clear_i()">Clear</button> </td>
   			</tr>
   		</table>`
 });
