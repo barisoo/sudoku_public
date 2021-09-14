091c4907bf116331a7868cf8b92591626cc0d68d
barisoo <bariso@mac.com>
2021-09-15 01:06:40 +1000

    Update sudoku_board.js

--- sudoku_board.js
+++ sudoku_board.js
@@ -1,10 +1,7 @@
 
 var cells = [];
-for (let i = 0; i<sb.cells.length; i++){
-	cells.push(sb.cells[i].val);
-}
 canvas = document.getElementById("gameArea");
-var currentCellIndex = 0;
+var currentCellIndex = null;
 var cellsPerAxis = 9;
 var cellWidth = (canvas.width) / cellsPerAxis;
 var cellHeight = (canvas.width) / cellsPerAxis;
@@ -13,28 +10,40 @@ var mouse_y = 0;
 var CSS_dimension = 360;
 var canvas_dimension = canvas.width;
 var CSSratio=  canvas_dimension/CSS_dimension;
+var highlightColor = "blue";
+var correctHighlight = "#58cc77";
+var falseHighlight = "#ed7c6d";
+var shouldCheckAnswers = false;
 
-canvas.addEventListener("click", function(e) { 
-	var cRect = canvas.getBoundingClientRect();        // Gets CSS pos, and width/height
-	var canvasX = Math.round(CSSratio*(e.clientX - cRect.left));
-	// console.log(cRect.left);  // Subtract the 'left' of the canvas 
-	var canvasY = Math.round(CSSratio*(e.clientY - cRect.top));
-	mouse_x = canvasX;
-	mouse_y = canvasY;
-	currentCellIndex = getCellIndex();
-    // from the X/Y positions to make  
-	//ctx.clearRect(0, 0, canvas.width, canvas.height);  // (0,0) the top left of the canvas
-	//ctx.fillText("X: "+canvasX+", Y: "+canvasY, 10, 20);
-});
+function loadCellVals(){
+	cells = [];
+	for (let i = 0; i<sb.cells.length; i++){
+	cells.push(sb.cells[i].val);
+}
+}
+function checkAnswers(){
+	let compareWithSolution = [];
+	for (let i =0; i<sb.cells.length;i++){
+		if(sb.solution[i] == cells[i]){
+			compareWithSolution.push(1);
+		} else {compareWithSolution.push(0);}
+	}
 
-/*function getMousePos(canvas, evt) {
-	canvas = document.getElementById("gameArea");
-var rect = canvas.getBoundingClientRect();
-return {
-	x: evt.clientX - rect.left,
-	y: evt.clientY - rect.top
- 	};
-}*/
+	for (let i=0; i<compareWithSolution.length;i++){
+		let row = Math.floor(i / 9);
+		let col = i - (row*9);
+		let strtx = col * cellWidth;
+		let strty = row * cellHeight;
+		ctx.lineWidth = 3*CSSratio;
+		canvas = document.getElementById("gameArea");
+		ctx = canvas.getContext('2d');
+		if (compareWithSolution[i] == 1){
+		ctx.fillStyle =correctHighlight;} else {ctx.fillStyle =falseHighlight;}
+		ctx.beginPath();
+		ctx.rect(strtx, strty, cellWidth, cellHeight);
+		ctx.fill();
+	}
+}
 
 function drawBoard(){
 	canvas = document.getElementById("gameArea");
@@ -43,6 +52,17 @@ function drawBoard(){
 	ctx.beginPath();
 	ctx.rect(0,0,(canvas.width),(canvas.height));
 	ctx.fill();
+	if (shouldCheckAnswers){checkAnswers()}
+	for(let i =0; i <3; i++){
+		ctx.strokeStyle ="grey";
+		ctx.lineWidth = 1*CSSratio;
+		ctx.beginPath();
+		ctx.rect(0,(canvas.height/9) + ((canvas.height/3)*i),(canvas.width),(canvas.height)/9);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.rect((canvas.width/9) + ((canvas.width/3)*i),0,(canvas.width/9),(canvas.height));
+		ctx.stroke();
+	}
 	ctx.strokeStyle ="black";
 	ctx.lineWidth = 3*CSSratio;
 	ctx.beginPath();
@@ -54,17 +74,6 @@ function drawBoard(){
 	ctx.beginPath();
 	ctx.rect(0,(canvas.height)/3,(canvas.width),(canvas.height)/3);
 	ctx.stroke();
-
-	for(let i =0; i <3; i++){
-		ctx.strokeStyle ="grey";
-		ctx.lineWidth = 1*CSSratio;
-		ctx.beginPath();
-		ctx.rect(0,(canvas.height/9) + ((canvas.height/3)*i),(canvas.width),(canvas.height)/9);
-		ctx.stroke();
-		ctx.beginPath();
-		ctx.rect((canvas.width/9) + ((canvas.width/3)*i),0,(canvas.width/9),(canvas.height));
-		ctx.stroke();
-	}
 }
 function getCellIndex(){ //later this should only update if there is a mouse click
 	//may seem unnecessary since the col and row are recalculated later, but the cell index is used in the logic script!
@@ -78,6 +87,7 @@ function getCellIndex(){ //later this should only update if there is a mouse cli
 }
 
 function highlightSelectedCell(){
+	if (currentCellIndex !=null){
 	let row = Math.floor(currentCellIndex / 9);
 	let col = currentCellIndex - (row*9);
 	let strtx = col * cellWidth;
@@ -85,20 +95,23 @@ function highlightSelectedCell(){
 	ctx.lineWidth = 3*CSSratio;
 	canvas = document.getElementById("gameArea");
 	ctx = canvas.getContext('2d');
-	ctx.strokeStyle ="red";
+	ctx.strokeStyle =highlightColor;
 	ctx.beginPath();
 	ctx.rect(strtx, strty, cellWidth, cellHeight);
 	ctx.stroke();
+	}
 }
 
 
 function displayNumbers(){
+	loadCellVals();
 	for (let i =0 ; i<cells.length; i++){
 		let row = Math.floor(i / 9);
 		let col = i - (row*9);
 		let strtx = (col + 0.3) * cellWidth ;
 		let strty = (row + 0.8) * cellHeight;
-		ctx.fillStyle = 'black';
+		if (sb.cells[i].isComputerAssigned){
+		ctx.fillStyle = 'black';} else {ctx.fillStyle = 'grey';}
 		let text = cells[i];
 		if (text!=0){
 		ctx.fillText(text, strtx, strty); 
@@ -123,3 +136,6 @@ function main(){
 }
 
 window.onLoad = main();
+
+
+
