5c3aeb8226f8a3b4c34709abffbd844a7311bb17
barisoo <bariso@mac.com>
2021-09-16 17:24:00 +1000

    Update numberpad.js

--- numberpad.js
+++ numberpad.js
@@ -4,13 +4,11 @@ let numberpad = Vue.createApp({
   				<td><button onclick="insertValFromInput(1)"> 1</button> </td>
   				<td><button onclick="insertValFromInput(2)"> 2</button> </td>
   				<td><button onclick="insertValFromInput(3)"> 3</button> </td>
-  			</tr>
-  			<tr>
+
   				<td> <button onclick="insertValFromInput(4)">4 </button></td>
   				<td> <button onclick="insertValFromInput(5)">5 </button></td>
   				<td> <button onclick="insertValFromInput(6)">6 </button></td>
-  			</tr>
-  			<tr>
+
   				<td> <button onclick="insertValFromInput(7)">7</button> </td>
   				<td> <button onclick="insertValFromInput(8)">8</button> </td>
   				<td> <button onclick="insertValFromInput(9)">9</button> </td>
